f583a79d24dc7fc9f7d513b451d1e5deaaa55b3a
JEFEMAESTRO10 <alexis10940649@gmail.com>
2024-12-10 16:50:57 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -23,6 +23,43 @@ def createTable():
     conn.commit()
     conn.close()
 
+def insertRow(experience_id, experience_type, start_date, end_date, budget=None, status="scheduled"):
+    conn = sql.connect("autoconocimientoKevin.db")
+    cursor = conn.cursor()
+    query = """
+    INSERT INTO experience_planning (experience_id, experience_type, start_date, end_date, budget, status)
+    VALUES (?, ?, ?, ?, ?, ?);
+    """
+    try:
+        cursor.execute(query, (experience_id, experience_type, start_date, end_date, budget, status))
+        conn.commit()
+        print("Datos insertados correctamente")
+    except sql.Error as e:
+        print(f"Error al insertar datos: {e}")
+    finally:
+        conn.close()
+
+def readRow():
+    try:
+        conn = sql.connect("autoconocimientoKevin.db")
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT * FROM experience_planning;
+        """)
+        rows = cursor.fetchall()
+        if rows:
+            print("Registros en la tabla experience_planning:")
+            for row in rows:
+                print(row)
+        else:
+            print("No hay registros en la tabla experience_planning.")
+    except sql.Error as e:
+        print(f"Error al leer los registros: {e}")
+    
+    finally:
+        conn.close()
+
+
 if __name__ == "__main__":
     createDB()
     createTable()
